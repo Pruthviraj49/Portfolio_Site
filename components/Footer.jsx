77cc6fc1614ab0784171ec3073757475d2a507b6
Pruthviraj49 <83855808+Pruthviraj49@users.noreply.github.com>
2023-01-28 15:08:26 +0530

    Updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <footer className="fixed bg-gray-700 text-white py-4 w-full bottom-0 font-chivoMono">
+            <footer className=" bg-gray-700 text-white py-4 w-full font-chivoMono">
                 <div className="container mx-auto">
                     <p className="text-center px-5">Copyright © {new Date().getFullYear()} Pruthviraj</p>
                 </div>
